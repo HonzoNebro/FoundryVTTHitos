d65e9cd4ccbf9d20e67f000cd02847f3262d391b
Honzo <honzo.nebro@gmaill.com>
2020-06-23 21:36:07 +0200

    Fixes on template and dice roll

--- module/actor/actor-sheet.js
+++ module/actor/actor-sheet.js
@@ -93,36 +93,44 @@ export class HitosActorSheet extends ActorSheet {
     event.preventDefault();
     const element = event.currentTarget;
     const dataset = element.dataset;
-    let r1 = new Roll("1d10").roll().result;
-    let r2 = new Roll("1d10").roll().result;
-    let r3 = new Roll("1d10").roll().result;
+    /*
+    let r1 = new Roll("1d1").roll().result;
+    let r2 = new Roll("2d1").roll().result;
+    let r3 = new Roll("3d1").roll().result;
     let mCM = [r1, r2, r3];
+    mCM = mCM.sort(function (a, b) { return a - b });
+    if (dataset.roll === 'mCM') {
+    }else if(dataset.roll === 'm'){
+      mCM.splice(1, 2);
+    } else if (dataset.roll === 'C') {
+      mCM.splice(0, 1);
+      mCM.splice(1, 1);
+    } else if (dataset.roll === 'M') {
+      mCM.splice(0, 2);
+    } else if (dataset.roll === 'mC') {
+      mCM.splice(2, 1);
+    } else if (dataset.roll === 'mM') {
+      mCM.splice(1, 1);
+    } else if (dataset.roll === 'CM') {
+      mCM.splice(0, 1);
+    }
     let label = dataset.label ? `Rolling ${dataset.label}` : '';
     ChatMessage.create({
       user: game.user._id,
       speaker: ChatMessage.getSpeaker({ actor: this.actor }),
       sound: CONFIG.sounds.dice,
-      content: mCM.sort(function (a, b) { return a - b }),
+      content: mCM,
       flavor: label
     });
-
-    /*if (dataset.roll) {
+    */
+    if (dataset.roll) {
       let roll = new Roll(dataset.roll, this.actor.data.data);
       let label = dataset.label ? `Rolling ${dataset.label}` : '';
-      roll.dice(new rolls(mCM(0), mCM(1), mCM(2)))
       roll.roll().toMessage({
         speaker: ChatMessage.getSpeaker({ actor: this.actor }),
         flavor: label
       });
-    } else {
-      let content = r1.render() + r2.render() + r3.render();
-      ChatMessage.create({
-        user: game.user._id,
-        speaker: ChatMessage.getSpeaker({ actor: this.actor }),
-        sound: CONFIG.sounds.dice,
-        content: content
-      });
-    }*/
+    }
   }
 
   /*{"class":"Roll","formula":"3d10","dice":
@@ -130,14 +138,5 @@ export class HitosActorSheet extends ActorSheet {
   [{"roll":5},{"roll":10},{"roll":6}],
   "formula":"3d10","options":{}}],
   "parts":["_d0"],"result":"21","total":21}*/
-
-
-  _sortmCM(r1, r2, r4) {
-    /*let r1 = new Roll("1d10").roll().result;
-    let r2 = new Roll("1d10").roll().result;
-    let r3 = new Roll("1d10").roll().result;*/
-    let mCM = [r1, r2, r3];
-    return mCM.sort(function (a, b) { return a - b });
-  }
 
 }
